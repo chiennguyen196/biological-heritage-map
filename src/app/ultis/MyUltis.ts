eaa1512a499b5c85b53d35fe8dde3380323d7d92
Chien Nguyen <chiennm3@cyberspace.vn>
2019-12-04 02:52:28 +0000

    remove khu bao ton

--- src/app/ultis/MyUltis.ts
+++ src/app/ultis/MyUltis.ts
@@ -29,7 +29,7 @@ export class MyUltis {
   }
 
   public static getColorOfFeature(type: DataType, feature: Feature): string {
-    if ([DataType.KHU_BAO_TON, DataType.KHU_DI_SAN, DataType.KHU_DU_TRU_SINH_QUYEN, DataType.VUON_QUOC_GIA].find((ele) => ele === type)) {
+    if ([DataType.KHU_DI_SAN, DataType.KHU_DU_TRU_SINH_QUYEN, DataType.VUON_QUOC_GIA].find((ele) => ele === type)) {
       return MyUltis.stringToColour(type + 'ABC');
     }
     return null;
